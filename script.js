a47c5ff87da55bace3a6fd98543394778485a270
Eckwain <74066027+Eckwain@users.noreply.github.com>
2021-05-13 22:19:48 +0500

    ZXC
    Da

---
+++ script.js
@@ -0,0 +1,9 @@
+// let otvet;
+// otvet = prompt('Ты гуль? (Ответ Да/Нет)');
+// if (otvet == "Да" || otvet == 'да'){
+//   alert('Доступ разрешен')
+// }
+// else {
+//   alert('Доступ запрещен')
+//   window.close();
+// }
